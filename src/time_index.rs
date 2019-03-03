879b89768e5cea71afc2972883071b1dddb98b1a
Olivier Pinon <oliv.pinon@gmail.com>
2019-03-03 21:55:27 +0100

    fix doc time_index

--- src/time_index.rs
+++ src/time_index.rs
@@ -8,18 +8,29 @@ pub struct TimeIndex {
 }
 
 impl TimeIndex {
+    /// Creates a new instance of the TimeIndex
     pub fn new(min: usize, max: usize, current: usize) -> Self {
         TimeIndex { min, max, current }
     }
 
+    /// Adds a maximum index
     pub fn add_max(&mut self) {
         self.max += 1;
     }
 
-    pub fn set_current(&mut self, new: usize) {
+    /// Sets the current time frame, checking the min and max bounds.
+    /// Returns:
+    /// * true if the operation was successful,
+    /// * false otherwise
+    pub fn set_current(&mut self, new: usize) -> bool {
+        if self.max < new || self.min > new {
+            return false;
+        }
         self.current = new;
+        true
     }
 
+    /// Moves the current time frame forward
     pub fn forward(&mut self) {
         self.current = match self.current.checked_add(1) {
             Some(new) => new,
@@ -27,6 +38,7 @@ impl TimeIndex {
         }
     }
 
+    /// Moves the current time frame backward
     pub fn backward(&mut self) {
         self.current = match self.current.checked_sub(1) {
             Some(new) => new,
